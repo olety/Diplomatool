9c7278af5e453d29d7218de39872c1f663042e48
Oleksii Kyrylchuk <olety7@gmail.com>
2017-02-03 04:33:09 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
-from pip.req import parse_requirements
-
-install_reqs = parse_requirements('requirements.txt')
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(name='ssd-project',
       version='0.1',
       description='ssd project by Oleksii and Piotr',
       author='Oleksii Kyrylchuk, Piotr Fudali',
       author_email='olkyrylchuk@gmail.com',
-      install_requires=reqs
+      install_requires=['alabaster',
+                        'Babel',
+                        'Django',
+                        'django-crispy-forms',
+                        'django-model-utils',
+                        'docutils',
+                        'imagesize',
+                        'Jinja2',
+                        'MarkupSafe',
+                        'Pygments',
+                        'pytz',
+                        'requests',
+                        'six',
+                        'snowballstemmer',
+                        'Sphinx',
+                        'wheel']
       )
