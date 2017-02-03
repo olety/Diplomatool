308eae199037b156953c4a635b2f2dc7dab4b50f
Oleksii Kyrylchuk <olety7@gmail.com>
2017-02-03 02:44:46 +0100

    Updated travis conf and sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'Diplomatool.settings'
 
 django.setup()
 from django.core.management import execute_from_command_line
-execute_from_command_line(['manage.py', 'makemigrations'])
 execute_from_command_line(['manage.py', 'migrate'])
 # sys.path.insert(0, os.path.abspath('.'))
 
