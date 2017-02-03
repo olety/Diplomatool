271153d19c517faa0c3b4e43794522592dc55d26
Oleksii Kyrylchuk <olety7@gmail.com>
2017-02-03 02:37:51 +0100

    Updated sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,14 @@
 import os
 import sys
 import django
+
 sys.path.insert(0, os.path.abspath('..'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Diplomatool.settings'
+
 django.setup()
+from django.core.management import execute_from_command_line
+execute_from_command_line(['manage.py', 'makemigrations'])
+execute_from_command_line(['manage.py', 'migrate'])
 # sys.path.insert(0, os.path.abspath('.'))
 
 # sys.path.insert(0, os.path.abspath('..'))
