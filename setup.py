d7f7c050b9dc78c764ea554524ae3e2f3d4792d8
Oleksii Kyrylchuk <olety7@gmail.com>
2017-02-03 01:55:42 +0100

    Added unit and integration tests

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ssd-project',
+      version='0.1',
+      description='ssd',
+      long_description='ssd project by Oleksii and Piotr',
+      author='Oleksii Kyrylchuk, Piotr Fudali',
+      author_email='olkyrylchuk@gmail.com',
+      license='None',
+      packages=['django'],
+      zip_safe=False,
+      install_requires=[
+          'Django',
+          ],
+      )
